aad02a41cc36210bfc33c7d08cfe43edcb5e116f
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-06-23 09:54:40 +0800

    Update contrastTemplate.ino

--- examples/contrastTemplate/contrastTemplate.ino
+++ examples/contrastTemplate/contrastTemplate.ino
@@ -1,6 +1,6 @@
 /*!
  *@file contrastTemplate.ino
- *@brief 将ID 1指纹模板提取出来，采集指纹与模板做对比
+ *@brief Extract the ID 1 fingerprint template, and collect fingerprint to compare with it.
  *@copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
  *@licence     The MIT License (MIT)
  *@author [fengli](li.feng@dfrobot.com)
@@ -12,7 +12,7 @@
 #include <DFRobot_ID809.h>
 
 
-uint8_t temp[1008]={0};    //模板数据
+uint8_t temp[1008]={0};    //Template data
 
 /*Use software serial when using UNO or NANO*/
 #if ((defined ARDUINO_AVR_UNO) || (defined ARDUINO_AVR_NANO))
@@ -44,19 +44,19 @@ void setup(){
     //Serial.println(desc);
     delay(1000);
   }
-  //获取ID 1的指纹模板数据
+  //Get the fingerprint template of ID 1
   fingerprint.getTemplate(/*id = */1,temp);
-  //将模板数据下载到ID 1中
+  //Download the template to ID 1.
   //fingerprint.downLoadTemplate(/*id = */1,temp);
 }
 
 void loop(){
-  /*设置指纹LED环为蓝色呼吸灯*/
+  /*Set the fingerprint LED ring as blue breathing light*/
   fingerprint.ctrlLED(/*LEDMode = */fingerprint.eBreathing, /*LEDColor = */fingerprint.eLEDBlue, /*blinkCount = */0);
   Serial.println("Please press down your finger");
   /*Capture fingerprint image, Disable the collection timeout function 
     If succeed return 0, otherwise return ERR_ID809
-    采集指纹模板数据,并储存到rambuffer0
+    Collect fingerprint template data and store them to rambuffer0
    */
   if((fingerprint.collectionFingerprint(/*timeout=*/0,0)) != ERR_ID809){
     /*Set fingerprint LED ring to quick blink in yellow 3 times*/
@@ -67,15 +67,15 @@ void loop(){
       Return 1 when finger is detected, otherwise return 0 
      */
     while(fingerprint.detectFinger());
-    //将模板数据与采集的指纹做对比
+    //Compare the template data with the collected fingerprints
     if(!fingerprint.contrastTemplate(/*TEMP = */temp)){
       /*Set fingerprint LED ring to always ON in green*/
       fingerprint.ctrlLED(/*LEDMode = */fingerprint.eKeepsOn, /*LEDColor = */fingerprint.eLEDGreen, /*blinkCount = */0);
-      Serial.println("Matching succeeds,模板与手指匹配");
+      Serial.println("Matching succeeds, the template matches the finger");
     }else{
       /*Set fingerprint LED ring to always ON in red*/
       fingerprint.ctrlLED(/*LEDMode = */fingerprint.eKeepsOn, /*LEDColor = */fingerprint.eLEDRed, /*blinkCount = */0);
-      Serial.println("Matching fails,模板手指不匹配");
+      Serial.println("Matching fails, the template does not match the finger");
     }
   }else{
     Serial.println("Capturing fails");
